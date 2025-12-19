5ce1da9ae646148fc5d7681704822a8f6e52ac9d
isyizhang <08280828zy@gmail.com>
2025-12-19 10:53:41 -0600

    change site name

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Evie Yi Zhang'
-export const SITE_DESCRIPTION = 'Evie Yi Zhang Personal Website '
+export const SITE_TITLE = 'Yi Evie Zhang'
+export const SITE_DESCRIPTION = 'Yi Evie Zhang Personal Website '
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
